e3becf96875ef0e6dbd506d3ff0c932c4d8b970d
aferris17 <aferris17@my.whitworth.edu>
2015-01-05 15:02:30 -0800

    spanish version

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -16,7 +16,7 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hola Mundo");
     }
     
 }
