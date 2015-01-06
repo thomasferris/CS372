dd58b56b6f6db96f8f661a0bbac487f1b1b939ab
aferris17 <aferris17@my.whitworth.edu>
2015-01-06 14:51:56 -0800

    PR1_2

---
+++ PR1_2/src/pr1_2/PR1_2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pr1_2;
+
+/**
+ *
+ * @author aferris17
+ */
+public class PR1_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i=0; i<3; i++)
+        {
+            System.out.printf("%s", args[i]);
+        }
+    }
+    }
+    
+
