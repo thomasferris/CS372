029f3fbf4b6ad802997c639a0233f8b25cea0721
aferris17 <aferris17@my.whitworth.edu>
2015-01-12 15:36:14 -0800

    PR3_1

---
+++ PR3_1/src/pr3_1/Calculate.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pr3_1;
+import java.util.*;
+
+/**
+ *
+ * @author aferris17
+ */
+public class Calculate {
+    Calculate (){}; 
+    
+    public int min (ArrayList<Integer> array){
+        int temp = array.get(0);
+        for (int i=1; i<array.size();i++){
+            if (temp > array.get(i)){
+                temp = array.get(i);
+            }
+        }
+        return temp;
+    }
+    
+    public int max (ArrayList<Integer>array){
+        int temp = array.get(0);
+        for (int i=1; i<array.size();i++){
+            if (temp < array.get(i)){
+                temp = array.get(i);
+            }
+        }
+        return temp;
+    }
+    
+    public Double avg (ArrayList<Integer>array){
+        Double total=0.0;
+        Double count=0.0;
+        for(int i=0;i<array.size();i++){
+            total+=(double)array.get(i);
+            count++;
+        }
+        
+        return total/count;
+    }
+    }
+
---
+++ PR3_1/src/pr3_1/PR3_1.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pr3_1;
+import java.util.*;
+
+/**
+ *
+ * @author aferris17
+ */
+public class PR3_1 {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculate cal = new Calculate();
+        System.out.println("Please enter 10 integers below:");
+        Scanner scan = new Scanner (System.in);
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        for (int i = 0; i < 10; i++) {
+            try {
+                Integer x = scan.nextInt();
+                numbers.add(x);
+            } catch (InputMismatchException e) {
+                System.out.println("Not a valid integer, please try again");
+                i--;
+                scan.nextLine();
+            }
+        }
+
+     
+        System.out.printf("Maximum: %d\n", cal.max(numbers));
+        System.out.printf("Minimum: %d\n", cal.min(numbers));
+        System.out.printf("Average: %4f\n", cal.avg(numbers));
+
+    }
+    
+}
